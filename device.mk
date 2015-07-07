c44746fd211a205dd1b2a34cfa26dc28fc0a6760
Dan Pasanen <dan.pasanen@gmail.com>
2015-07-06 19:01:49 -0500

    wcnss: update firmware and configs from VK810 35A
    Change-Id: Ifc2822e7e05d679fefee411415c5348a16b9d8f0

--- device.mk
+++ device.mk
@@ -51,7 +51,8 @@ PRODUCT_COPY_FILES += \
         $(LOCAL_PATH)/WCNSS_cfg.dat:system/vendor/firmware/wlan/prima/WCNSS_cfg.dat \
         $(LOCAL_PATH)/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
         $(LOCAL_PATH)/WCNSS_qcom_cfg.ini:system/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
-        $(LOCAL_PATH)/WCNSS_qcom_wlan_nv.bin:system/etc/wifi/WCNSS_qcom_wlan_nv.bin
+        $(LOCAL_PATH)/WCNSS_qcom_wlan_nv.bin:system/etc/wifi/WCNSS_qcom_wlan_nv.bin \
+        $(LOCAL_PATH)/WCNSS_qcom_wlan_nv_init.bin:system/vendor/firmware/wlan/prima/WCNSS_qcom_wlan_nv_init.bin
 
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/audio_policy.conf:system/etc/audio_policy.conf
