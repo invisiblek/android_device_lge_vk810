833180816d855d0636a03129e0bab8ff395d2b8f
Michael Bestas <mikeioannina@gmail.com>
2015-11-29 21:40:02 -0600

    vk810: remove unused packages
    * Remove packages that are unused, already included in build or vendor
    
    Change-Id: I05f5d7806b6338f5b2b5b9529fb5c38c75867aba

--- device.mk
+++ device.mk
@@ -128,12 +128,7 @@ PRODUCT_PROPERTY_OVERRIDES += persist.hwc.mdpcomp.enable=true
 PRODUCT_TAGS += dalvik.gc.type-precise
 
 PRODUCT_PACKAGES += \
-	librs_jni \
-	com.android.future.usb.accessory
-
-# Filesystem management tools
-PRODUCT_PACKAGES += \
-	e2fsck
+	librs_jni
 
 PRODUCT_PACKAGES += \
 	libgenlock \
