78d977003420d5f07726a2e777b2e82ca364dbc8
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-01-14 07:38:42 -0600

    Revert "vk810: inherit full_base_telephony"
    This reverts commit 9d1b68e7301b86fbb951ca048d06ccd521cf8531.

--- cm.mk
+++ cm.mk
@@ -6,7 +6,6 @@ TARGET_SCREEN_WIDTH := 1200
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 $(call inherit-product, vendor/cm/config/telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/vk810/vk810.mk)
