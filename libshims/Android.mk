c5f6dfe8997f032ff1ab2be34dc3ed24392ccc37
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-07-05 18:05:59 -0500

    vk810: update camera shim for L-based camera hal
    Change-Id: Ie40cd6e974e30a645c8e77c54e75554fb72ac363

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -2,18 +2,9 @@ LOCAL_PATH := $(call my-dir)
 
 # camera
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := \
-     gui/SensorManager.cpp \
-     ui/GraphicBuffer.cpp \
-     ui/GraphicBufferAllocator.cpp \
-     ui/GraphicBufferMapper.cpp \
-     MemoryHeapPmem.cpp \
-     MemoryBase.c \
-     VectorImpl.c
-LOCAL_SHARED_LIBRARIES := liblog libcutils libhardware libui libgui libbinder libutils libsync
+LOCAL_SRC_FILES := libshim_camera.c
+LOCAL_SHARED_LIBRARIES := libutils libgui
 LOCAL_MODULE := libshim_camera
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
 
