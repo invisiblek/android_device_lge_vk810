f58ed835b2f048cffe28efd62dae10aa38144747
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-09-15 12:33:31 -0500

    vk810: remove sepolicy
    * This device will never get official builds and
      I have no ambition to fix these properly. Ever.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,10 +107,6 @@ TARGET_RECOVERY_FSTAB = device/lge/vk810/rootdir/etc/fstab.altev
 TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
-# SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += device/lge/vk810/sepolicy
-
 # Time Services
 BOARD_USES_QC_TIME_SERVICES := true
 
