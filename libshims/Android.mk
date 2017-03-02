7faf1e559911ee83c74ee8e8703db073b53a46a2
fefifofum <jorsellis@gmail.com>
2017-03-01 23:44:11 -0600

    vk810: Cleanup camera shim

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 # camera
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := libshim_camera.c
-LOCAL_SHARED_LIBRARIES := libutils libgui
+LOCAL_SHARED_LIBRARIES := libutils
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
