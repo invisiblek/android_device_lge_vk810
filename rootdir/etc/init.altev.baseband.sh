2cc94324df08e7363c1524bc4fbb999eeff22293
Dan Pasanen <dan.pasanen@gmail.com>
2017-03-04 08:46:18 -0600

    vk810: inherit gpad83-common

--- rootdir/etc/init.altev.baseband.sh
+++
@@ -1,5 +0,0 @@
-#!/system/bin/sh
-
-# hax hax hax
-# grep the modem partition for baseband version
-setprop gsm.version.baseband `strings /dev/block/platform/msm_sdcc.1/by-name/modem | grep "^M9615A\-" | head -1`
