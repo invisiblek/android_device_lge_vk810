f872e23b8d5c2d165ccde5ef937b5d76e7100fd4
Dan Pasanen <dan.pasanen@gmail.com>
2019-09-05 12:10:32 -0500

    vk810: vendorsetup -> COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,3 +15,7 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_vk810.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_vk810-eng \
+    lineage_vk810-userdebug
--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-add_lunch_combo lineage_vk810-eng
-add_lunch_combo lineage_vk810-userdebug
