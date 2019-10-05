0f02622e9e4146647f4cbffc24d0fd8d5d415afb
Dan Pasanen <dan.pasanen@gmail.com>
2019-10-05 11:45:15 -0500

    vk810: remove more telephony-related props

--- system_prop.mk
+++ system_prop.mk
@@ -2,13 +2,6 @@
 # system.prop for vk810
 #
 PRODUCT_PROPERTY_OVERRIDES += \
-    rild.libpath=/vendor/lib/libril-qc-qmi-1.so \
-    ro.telephony.default_network=9 \
-    persist.radio.apm_sim_not_pwdn=1 \
-    ril.subscription.types=NV,RUIM \
-    ro.ril.transmitpower=true \
-    persist.qcril.disable_retry=true \
-    telephony.lteOnGsmDevice=1 \
     ro.radio.noril=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
